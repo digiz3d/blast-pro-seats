a34d25c8eeabee2cb2c3ab5aa98934d71aa9f033
digiz3d <1338620+digiz3d@users.noreply.github.com>
2019-02-03 23:02:51 +0100

    real countdown on the first screen

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -22,11 +22,66 @@ const styles = StyleSheet.create({
   },
 });
 export default class Countdown extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      seconds: 35 * 60 + 18,
+      time: this.secondsToTime(35 * 60 + 18),
+    };
+
+    this.timer = 0;
+    this.countDown = this.countDown.bind(this);
+  }
+
+  componentDidMount() {
+    if (this.timer === 0 && this.state.seconds > 0) {
+      this.timer = setInterval(this.countDown, 1000);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  secondsToTime(secs) {
+    const hours = Math.floor(secs / (60 * 60));
+
+    const divisorForMinutes = secs % (60 * 60);
+    const minutes = Math.floor(divisorForMinutes / 60);
+
+    const divisorForSeconds = divisorForMinutes % 60;
+    const seconds = Math.ceil(divisorForSeconds);
+
+    const obj = {
+      h: ('0' + hours).slice(-2),
+      m: ('0' + minutes).slice(-2),
+      s: ('0' + seconds).slice(-2),
+    };
+    return obj;
+  }
+
+  countDown() {
+    const seconds = this.state.seconds - 1;
+    this.setState({
+      time: this.secondsToTime(seconds),
+      seconds: seconds,
+    });
+
+    if (seconds === 0 && this.timer !== 0) {
+      clearInterval(this.timer);
+    }
+  }
+
   render() {
     return (
       <View style={styles.purpleView}>
         <Text style={[styles.text, styles.mainText]}>
-          First quiz in 00:32:43
+          First quiz in {this.state.time.h}:{this.state.time.m}:
+          {this.state.time.s}
         </Text>
         <TouchableOpacity>
           <Text style={[styles.text, styles.secondaryText]}>Register now</Text>
